70b41e5749fb97bccdf1ae498fb65c828e1fbe1f
Moksh-123 <20bmiit035@gmail.com>
2024-04-27 11:21:21 +0530

    27 april

---
+++ C Pro/C1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+// int main(){
+// 	int a = 10, b = 20, c;
+
+// 	c=a; //value given to c c=10 b=20 a=
+// 	a=b; // value given a c=10 b= a=20
+// 	b=c;
+
+// 	printf("a %d",a);
+// 	printf("b %d",b); 
+	
+// } //With third variable
+
+int main(){
+
+int a, b ,i;
+
+
+
+scanf("%d",&a);
+scanf("%d",&b);
+i=a*b;
+
+}
